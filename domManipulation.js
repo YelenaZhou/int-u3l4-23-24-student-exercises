b7db6e59d8a8bbd4a65f45410716561ea2507af7
YelenaZhou <yelenazhou2021@gmail.com>
2024-06-11 14:07:52 -0400

    complete

--- domManipulation.js
+++ domManipulation.js
@@ -22,24 +22,28 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
+    myName.innerHTML = "Yelena";
+    myHobby.innerHTML = "read";
+    myLocation.innerHTML = "there";
 
-
-
-
-
-
-});
+})
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
-
-
-
+image1Button.addEventListener("click", function() {
+    image.src = "https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2.png"
+})
 
 
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
-
+image2Button.addEventListener("click", function() {
+    image.src = "https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+})
 
 // BONUS
 // 7. Try using different event types for your buttons.
+contentButton.addEventListener('mouseover', function() {
+    myName.innerHTML = "Y";
+
+})
